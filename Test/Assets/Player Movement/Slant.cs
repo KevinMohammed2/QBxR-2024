7b2f0c64b82a3a13e4837690677cc6edebcd2854
KevinMohammed2 <ke602967@ucf.edu>
2024-09-16 22:04:28 -0400

    Slant WR and CB
    Let me know if the collision logic works and the thoughts on the routes

---
+++ Test/Assets/Player Movement/Slant.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float yardDist = 5f;
+    public float slantAngle = 45f;
+    public bool slanting = false;
+    private Vector3 startPos;
+    private float timeElasped = 0f;
+    public float stopTime = 4f;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElasped += Time.deltaTime;
+        if (timeElasped < stopTime)
+        {
+            if (!slanting)
+            {
+                float distCover = Vector3.Distance(startPos, transform.position);
+                if (distCover < yardDist)
+                {
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                else
+                {
+                    slanting = true;
+                }
+            }
+            else
+            {
+                Vector3 slantDirection = Quaternion.Euler(0, slantAngle, 0) * Vector3.forward;
+                transform.Translate(slantDirection * speed * Time.deltaTime);   
+            }
+        }
+
+    }
+}
