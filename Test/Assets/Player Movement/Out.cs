0dbd0c8f2ab4167deb13d1b7b6036874644ca430
Ryan Weisman <ry326542@ucf.edu>
2024-09-23 18:57:41 -0400

    Updated Player Movement

--- Test/Assets/Player Movement/Out.cs
+++ Test/Assets/Player Movement/Out.cs
@@ -2,42 +2,42 @@ using UnityEngine;
 
 public class OutRoute : MonoBehaviour
 {
-    public float speed = 5f;
-    public float yardDist = 10f;
-    public float slantAngle = 90f;
-    public bool ninetyCut = false;
-    private Vector3 startPos;
-    private float timeElasped = 0f;
-    public float stopTime = 4f;
+  public float speed = 5f;
+  public float yardDist = 10f;
+  public float slantAngle = 90f;
+  public bool ninetyCut = false;
+  private Vector3 startPos;
+  private float timeElasped = 0f;
+  public float stopTime = 4f;
 
-    void Start()
-    {
-        startPos = transform.position;
-    }
+  void Start()
+  {
+    startPos = transform.position;
+  }
 
-    void Update()
+  void Update()
+  {
+    timeElasped += Time.deltaTime;
+    if (timeElasped < stopTime)
     {
-        timeElasped += Time.deltaTime;
-        if (timeElasped < stopTime)
+      if (!ninetyCut)
+      {
+        float distCover = Vector3.Distance(startPos, transform.position);
+        if (distCover < yardDist)
         {
-            if (!ninetyCut)
-            {
-                float distCover = Vector3.Distance(startPos, transform.position);
-                if (distCover < yardDist)
-                {
-                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                }
-                else
-                {
-                    ninetyCut = true;
-                }
-            }
-            else
-            {
-                Vector3 cutDirection = Quaternion.Euler(0, slantAngle, 0) * Vector3.forward;
-                transform.Translate(cutDirection * speed * Time.deltaTime);
-            }
+          transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
-        
+        else
+        {
+          ninetyCut = true;
+        }
+      }
+      else
+      {
+        Vector3 cutDirection = Quaternion.Euler(0, slantAngle, 0) * Vector3.forward;
+        transform.Translate(cutDirection * speed * Time.deltaTime);
+      }
     }
+
+  }
 }
