1fc495e23d03ce646b0e0df44582963df8b1a2d4
Ryan Weisman <ry326542@ucf.edu>
2024-09-24 14:32:22 -0400

    Updated football tracking

--- Test/Assets/Football/Scripts/FootballSpin.cs
+++ Test/Assets/Football/Scripts/FootballSpin.cs
@@ -6,16 +6,12 @@ public class FootballSpin : MonoBehaviour
   public float spinForce = 500f;  // Adjust this to control how fast the ball spins
   private Rigidbody rb;
   private bool isThrown = false;  // Tracks if the ball has been thrown
-  private bool playStarted = false; // Tracks if the play has started
-
-  private bool isCompleted = false;  // Tracks if the pass has been completed
-
-  private GameManager gameManager;
+  //private GameManager gameManager;
 
   private void Start()
   {
     rb = GetComponent<Rigidbody>();  // Get the Rigidbody attached to the football
-    gameManager = FindObjectOfType<GameManager>();  // Find the GameManager object in the scene
+    //gameManager = FindObjectOfType<GameManager>();  // Find the GameManager object in the scene
   }
 
   private void FixedUpdate()
@@ -31,7 +27,6 @@ public class FootballSpin : MonoBehaviour
   public void OnSelectExited(SelectExitEventArgs args)
   {
     isThrown = true;  // Start spinning when the ball is released
-    playStarted = true; // Mark the play as started
   }
 
   // Called when the ball is grabbed again
@@ -47,14 +42,12 @@ public class FootballSpin : MonoBehaviour
     if (collision.gameObject.CompareTag("FootballField") || collision.gameObject.CompareTag("GoldTeam"))
     {
       isThrown = false;  // Stop spinning when the football hits the ground
-      isCompleted = false;  // Mark the pass as completed
-      gameManager.ShowDialog("Incomplete Pass!");  // End the game with a message
+      //gameManager.ShowDialog("Incomplete Pass!");  // End the game with a message
     }
     else if (collision.gameObject.CompareTag("BlackTeam"))
     {
       isThrown = false;  // Stop spinning when the football hits the ground
-      isCompleted = true;  // Mark the pass as incomplete
-      gameManager.ShowDialog("Pass Completed!");  // End the game with a message
+      //gameManager.ShowDialog("Pass Completed!");  // End the game with a message
     }
   }
 }
